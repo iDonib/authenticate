521c96c4b34f720a59c599002bb7cde46ab73547
donib-irakihda <donib.irakihda@gmail.com>
2022-02-14 22:31:06 +0545

    Finished coding

--- donib/authentication/urls.py
+++ donib/authentication/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('signup', views.signup, name="signup"),
     path('signin', views.signin, name="signin"),
     path('signout', views.signout, name="signout"),
+    path('activate/<uidb64>/<token>', views.activate, name="activate"),
 ]
--- donib/authentication/views.py
+++ donib/authentication/views.py
@@ -1,6 +1,8 @@
+from base64 import urlsafe_b64encode
 from distutils.log import error
 from email import message
 import imp
+from lib2to3.pgen2.tokenize import generate_tokens
 import re
 from django.conf import settings
 from django.forms import PasswordInput
@@ -12,8 +14,13 @@ from django.contrib import messages
 from django.contrib.auth import authenticate
 from django.contrib.auth import login, logout
 from donib import settings
-from django.core.mail import send_mail
+from django.core.mail import EmailMessage, send_mail
 from django.conf import settings
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes, force_text
+from . tokens import generate_token
 
 # Create your views here.
 def home(request):
@@ -34,9 +41,9 @@ def signup(request):
             messages.error(request, 'Username already taken! Choose some other username!')
             return redirect('home')
         
-        # if User.objects.filter(email=email):
-        #     messages.error(request, "This Email is already registered")
-        #     return redirect('home')
+        if User.objects.filter(email=email):
+            messages.error(request, "This Email is already registered")
+            return redirect('home')
         
         if len(username)>10:
             messages.error(request, "Only 10 or less characters for username")
@@ -52,7 +59,7 @@ def signup(request):
         myuser = User.objects.create_user(username, email, pass1)
         myuser.first_name = fname
         myuser.last_name = lname
-
+        myuser.is_active = False
         myuser.save()
 
         messages.success(request, "Your account has been created successfully! \n Please check your email for confirming your account!")
@@ -63,6 +70,26 @@ def signup(request):
         from_email = settings.EMAIL_HOST_USER
         to_list = [myuser.email]
         send_mail(subject, message, from_email, to_list, fail_silently=True)
+
+        # Email Confirmation email
+
+        current_site = get_current_site(request)
+        email_subject = "Confirm your email at Donib's Page!"
+        message2 = render_to_string("email_confirmation.html", {
+            'name': myuser.first_name,
+            'domain': current_site.domain,
+            'uid': urlsafe_base64_encode(force_bytes(myuser.pk)),
+            'token': generate_token.make_token(myuser)
+        })
+
+        email = EmailMessage(
+            email_subject,
+            message2,
+            settings.EMAIL_HOST_USER,
+            [myuser.email],
+        )
+        email.fail_silently = True
+        email.send()
 
         return redirect('signin')
         
@@ -90,3 +117,18 @@ def signout(request):
     logout(request)
     messages.success(request, "Signed Out Successfully!")
     return redirect('home')
+
+def activate(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        myuser = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        myuser = None
+
+    if myuser is not None and generate_token.check_token(myuser, token):
+        myuser.is_active = True
+        myuser.save()
+        login(request, myuser)
+        return redirect('home')
+    else:
+        return render(request, 'activation_failed.html')
